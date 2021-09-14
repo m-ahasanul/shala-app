bcb81c545ca4f482539e9e99868f2c2c2db2dd03
ahasanul <ahasanul@example.com>
2021-09-14 21:36:55 +0600

    done adding

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.feature-carosel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3,
+        },
+        600:{
+            items:3,
+        },
+        1000:{
+            items:3,
+        }
+    }
+})
