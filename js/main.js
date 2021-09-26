c7063d5bf858dfef68edfad24bcc910ff85dd15e
ahasanul <ahasanul@example.com>
2021-09-26 16:24:36 +0600

    new some

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $('.feature-carosel').owlCarousel({
     loop:true,
     margin:10,
+    autoplay: true,
     responsiveClass:true,
     responsive:{
         0:{
@@ -13,4 +14,40 @@ $('.feature-carosel').owlCarousel({
             items:3,
         }
     }
-})
+});
+
+$('.testimonials-carosel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+$('.team-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
